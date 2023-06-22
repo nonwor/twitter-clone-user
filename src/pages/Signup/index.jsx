91b437466bb0b5f560be66e672b442d1bf76b902
Sataporn Worasilpchai <sworasilpchai@LABA.PSWAS.LOCAL>
2023-06-21 17:13:15 -0700

    UI

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 const Signup = () => {
+
+  const[email, setEmail] = useState('')
+  const[password, setPassword] = useState('')
+  const[passwordConfirm, serPasswordConfirm] = useState('')
+
+  
+  const handleForm = () =>{
+
+  }
+
   return (
-    <div>Signup</div>
+    <div>Signup
+      {/* <input>email</input> */}
+      <div>
+        <form>
+        <label>
+            Email
+            <input type="text" />
+          </label>
+          <label>
+            Password
+            <input type="password" />
+          </label>
+          <label>
+            Confirm Password
+            <input type="password" />
+          </label>
+
+          <input type="button" value="Submit" onClick={handleForm} />
+        </form>
+      </div>
+    </div>
+
   )
 }
 
